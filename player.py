c13476cb7f10a9de188295f9fd5f7fe7d6938a87
hwlu <haiwei.lu@99wuxian.com>
2017-06-05 16:33:31 +0800

    PEP-8 normalization

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 ﻿#!/usr/bin/python3
-# -*- conding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import os
 import shutil
 import sqlite3
@@ -7,13 +7,14 @@ import sys
 import math
 import json
 
-from bottle import *#pip install bottle
+from bottle import *  # pip install bottle
 
-db = lambda: sqlite3.connect('player.db')#define DB connection
+db = lambda: sqlite3.connect('player.db')  # define DB connection
 # def time_format(time):#turn seconds into hh:mm:ss time format
     # m, s = divmod(time, 60)
     # h, m = divmod(time/60, 60)
     # return "%02d:%02d:%02d" % (h, m, s)
+
 
 def get_size(file):
     size = os.path.getsize('./static/mp4/%s' % file)
@@ -22,13 +23,12 @@ def get_size(file):
     if size < 1024:
         return '%dB' % size
     else:
-        unit = ['B','KB','MB','GB','TB','PB','EB','ZB','YB','BB']
-        l = int(math.floor(math.log(size,1024)))
-        if l > 9:
-            l=9
-        return '%.1f%s' % (size/1024.0**l,unit[l])
+        unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'BB']
+        l = min(int(math.floor(math.log(size, 1024))), 9)
+        return '%.1f%s' % (size/1024.0**l, unit[l])
 
-def initdb():#initialize database by create history table
+
+def initdb():  # initialize database by create history table
     conn = db()
     conn.execute('''create table if not exists history
         (FILENAME TEXT PRIMARY KEY    NOT NULL,
@@ -37,6 +37,7 @@ def initdb():#initialize database by create history table
         LATEST_DATE DATETIME NOT NULL);''')
     conn.close()
     return
+
 
 def update_history_from_db(filename, time, duration):
     conn = db()
@@ -47,18 +48,20 @@ def update_history_from_db(filename, time, duration):
     conn.close()
     return
 
+
 def load_history_from_db(name):
     if not name:
         return
     conn = db()
-    cursor=conn.execute("select TIME from history where FILENAME=?",(name,))
+    cursor = conn.execute("select TIME from history where FILENAME=?", (name,))
     try:
-        time = cursor.fetchone()[0]
+        progress = cursor.fetchone()[0]
     except Exception as e:
         print(str(e))
-        time = ''
+        progress = ''
     conn.close()
-    return time
+    return progress
+
 
 def remove_history_from_db(name = None):
     conn = db()
@@ -96,16 +99,18 @@ def remove_history_from_db(name = None):
     # else:
         # return '<tr><td>Empty...</td></tr>'
 
+
 def history_list_json_from_db():
     conn = db()
     historys=conn.execute('''
         select * from history order by LATEST_DATE desc''').fetchall()
     conn.close()
-    dict=[{'filename':s[0], 'time':s[1], 'duration':s[2], 'latest_date':s[3],
-    'path':'/' + os.path.dirname(s[0])} for s in historys]
-    return json.dumps(dict)
+    history = [{'filename': s[0], 'time': s[1], 'duration': s[2], 'latest_date': s[3],
+    'path': '/' + os.path.dirname(s[0])} for s in historys]
+    return json.dumps(history)
 
-@route('/player.php')#index
+
+@route('/player.php')  # index
 def videoplayer():
     action = request.query.action
     src = request.query.src
@@ -129,7 +134,7 @@ def videoplayer():
         if not os.path.exists(dir_old):
             os.mkdir(dir_old)
         try:
-            shutil.move(file,dir_old)#gonna do something when file is occupied
+            shutil.move(file, dir_old)  # gonna do something when file is occupied
         except Exception as e:
             abort(404,str(e))
         return folder(os.path.dirname(src))
@@ -140,7 +145,8 @@ def videoplayer():
     else:
         title = 'Light mp4 Player'
     return template(
-    'player', src = src, progress = load_history_from_db(src), title = title)
+    'player', src=src, progress=load_history_from_db(src), title=title)
+
 
 @route('/suspend.php')
 def suspend():
@@ -152,8 +158,8 @@ def suspend():
         else:
             return 'Suspend Failure!'
     else:
-        # os.system("sudo /sbin/shutdown -h now")
         return 'OS not supported!'
+
 
 @route('/shutdown.php')
 def shutdown():
@@ -162,6 +168,7 @@ def shutdown():
     else:
         os.system("sudo /sbin/shutdown -h now")
 
+
 @route('/restart.php')
 def restart():
     if sys.platform == 'win32':
@@ -169,25 +176,29 @@ def restart():
     else:
         os.system("sudo /sbin/shutdown -r now")
 
-@route('/static/<file:re:.*>')#static files access
+
+@route('/static/<file:re:.*>')  # static files access
 def static(file):
     return static_file(file, root='./static')
 
-@route('/<file:re:.*\.((?i)mp)4$>')#mp4 static files access.
-#to support larger files(>2GB), you should use apache "AliasMatch"
+
+@route('/<file:re:.*\.((?i)mp)4$>')  # mp4 static files access.
+# to support larger files(>2GB), you should use apache "AliasMatch"
 def mp4(file):
     return static_file(file, root='./static/mp4')
 
-@route('/fs/<file:re:.*\.((?i)mp)4$>')#mp4 static files access.
-#to support larger files(>2GB), you should use apache "AliasMatch"
+
+@route('/fs/<file:re:.*\.((?i)mp)4$>')  # mp4 static files access.
+# to support larger files(>2GB), you should use apache "AliasMatch"
 def fs_mp4(file):
     return static_file(file, root='./static/mp4')
 
-@route('/<dir:re:.*>')#static folder access
+
+@route('/<dir:re:.*>')  # static folder access
 def folder(dir):
     try:
-        html_dir,html_mp4,html_files='','',''
-        if dir!='':
+        html_dir, html_mp4, html_files = '', '', ''
+        if dir != '':
             dirs=dir.split('/')
             html_dir='''
             <tr><td colspan=3>
@@ -199,7 +210,7 @@ def folder(dir):
               </li>
               '''
             for n,i in enumerate(dirs[:-1:],1):
-                print("/%s %s"%('/'.join(dirs[0:n]),i))
+                print("/%s %s" % ('/'.join(dirs[0:n]),i))
                 html_dir += '''
                 <li><span class="filelist dir" title="/%s">%s</span>
                 </li>''' % ('/'.join(dirs[0:n]),i)
@@ -216,11 +227,11 @@ def folder(dir):
         for file in os.listdir('./static/mp4/%s' % dir):
             if os.path.isdir('./static/mp4/%s%s' % (dir,file)):
                 html_dir += '''
-				<tr>
-				  <td><i class="glyphicon glyphicon-folder-close"></i></td>
-				  <td class="filelist dir" title="/%s%s">%s</td>
-				  <td class="move" title="%s%s">
-					<i class="glyphicon glyphicon-remove-circle" title="%s%s">
+                <tr>
+                  <td><i class="glyphicon glyphicon-folder-close"></i></td>
+                  <td class="filelist dir" title="/%s%s">%s</td>
+                  <td class="move" title="%s%s">
+                    <i class="glyphicon glyphicon-remove-circle" title="%s%s">
 					</i>
 				  </td>
 				</tr>''' % (dir, file, file, dir, file, dir, file)
@@ -254,9 +265,9 @@ def folder(dir):
     except Exception as e:
         abort(404,str(e))
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))#set file path as current
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # set file path as current
 initdb()
 
 if __name__=="__main__":
-    os.system('start http://127.0.0.1:8081/player.php')#open the page automatic
-    run(host='0.0.0.0', port=8081, debug=True)#you can change port here
+    os.system('start http://127.0.0.1:8081/player.php')  # open the page automatic
+    run(host='0.0.0.0', port=8081, debug=True)  # you can change port here
