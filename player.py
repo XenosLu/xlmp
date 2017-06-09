b2f1de47858d8bda120b73e0084c98f6f9b29c3e
Xenocider <xenos.lu@gmail.com>
2017-06-09 20:42:40 +0800

    Cleared up

--- player.py
+++ player.py
@@ -14,12 +14,8 @@ from bottle import route, run, template, static_file, abort, request, redirect  
 MP4_PATH = './static/mp4'  # mp4 file path
 
 
-def db():
-    return sqlite3.connect('player.db')  # define DB connection
-
-
 def run_sql(sql, *args):  # run SQL
-    conn = db()
+    conn = sqlite3.connect('player.db')  # define DB connection
     try:
         cursor = conn.execute(sql, args)
         result = cursor.fetchall()
@@ -29,13 +25,6 @@ def run_sql(sql, *args):  # run SQL
     except Exception as e:
         print(str(e))
         result = []
-    # print('%s %d' % (sql,cursor.rowcount))
-    # for i in ['delete', 'replace']:
-        # if sql.lower().startswith(i):
-            # conn.commit()
-            # break
-    # if not result:
-        # conn.commit()
     finally:
         conn.close()
     return result
@@ -53,63 +42,24 @@ def get_size(filename):
         return '%.1f%s' % (size/1024.0**l, unit[l])
 
 
-def init_db():  # initialize database by create history table
-    run_sql('''create table if not exists history
-                    (FILENAME text PRIMARY KEY not null,
-                    TIME float not null,
-                    DURATION float, LATEST_DATE datetime not null);''')
-    return
-
-
-def update_from_history_db(filename, time, duration):
-    run_sql('''replace into history (FILENAME, TIME, DURATION, LATEST_DATE)
-                     values(? , ?, ?, DateTime('now', 'localtime'));''', filename, time, duration)
-    return
+# def init_db():  # initialize database by create history table
+    # run_sql('''create table if not exists history
+                    # (FILENAME text PRIMARY KEY not null,
+                    # TIME float not null,
+                    # DURATION float, LATEST_DATE datetime not null);''')
+    # return
 
 
 def load_from_history_db(name):
-    if not name:
-        return 0
-    # conn = db()
-    # cursor = conn.execute('select TIME from history where FILENAME=?', (name,))
     progress = run_sql('select TIME from history where FILENAME=?', name)
-    # progress = cursor.fetchone()
-    # cursor.close()
-    # conn.close()
-    # return str(len(progress))
     if len(progress) == 0:
         return 0
-    # else
-    return str(progress[0][0])
-    # if progress:
-        # return progress[0][0]
-    # return 0
-    # try:
-        # progress = cursor.fetchone()[0]
-    # except Exception as e:
-        # print(str(e))
-        # progress = ''
-
-
-def remove_from_history_db(name=None):
-    # conn = db()
-    if name:
-        run_sql('delete from history where FILENAME= ?', name)
-        # conn.execute('delete from history where FILENAME= ?', (name,))
-    else:
-        # conn.execute('delete from history')  # clear all
-        run_sql('delete from history')  # clear all
-    # conn.commit()
-    # conn.close()
-    return
+    return progress[0][0]
 
 
 @route('/list')  # list play history
 def list_from_history_db():
-    # conn = db()
-    # historys = conn.execute('select * from history order by LATEST_DATE desc').fetchall()
     historys = run_sql('select * from history order by LATEST_DATE desc')
-    # conn.close()
     history = [{'filename': s[0], 'time': s[1], 'duration': s[2], 'latest_date': s[3],
                 'path': os.path.dirname(s[0])} for s in historys]
     return json.dumps(history)
@@ -127,15 +77,16 @@ def play(src):
     return template('player', src=src, progress=load_from_history_db(src), title=src)
 
 
-@route('/clear')  # clear play history
+@route('/clear')
 def clear():
-    remove_from_history_db()
+    'Clear play history'
+    run_sql('delete from history')
     return list_from_history_db()
 
 
 @route('/remove/<src:path>')  # clear from play history
 def remove(src):
-    remove_from_history_db(src)
+    run_sql('delete from history where FILENAME= ?', src)
     return list_from_history_db()
 
 
@@ -158,7 +109,8 @@ def save(src):
     # src = request.query.src
     progress = request.GET.get('progress')
     duration = request.GET.get('duration')
-    update_from_history_db(src, progress, duration)
+    run_sql('''replace into history (FILENAME, TIME, DURATION, LATEST_DATE)
+               values(? , ?, ?, DateTime('now', 'localtime'));''', src, progress, duration)
     return
 
 
@@ -223,77 +175,35 @@ def fs_dir(path):
     except Exception as e:
         abort(404, str(e))
 
-
-# @route('/<path:re:.*>')  # static folder access
 # def folder(path):
-    # try:
-        # html_dir, html_mp4, html_files = '', '', ''
-        # if path != '':
-            # dirs = path.split('/')
-            # html_dir = '''
-            # <tr><td colspan=3>
-            # <ol class="breadcrumb">
-              # <li>
-                # <span class="filelist folder">
-                  # <i class="glyphicon glyphicon-home" title="/"></i>
-                # </span>
-              # </li>
-              # '''
-            # for n, i in enumerate(dirs[:-1:], 1):
-                # print("/%s %s" % ('/'.join(dirs[0:n]), i))
-                # html_dir += '''
-                # <li><span class="filelist folder" title="/%s">%s</span>
-                # </li>''' % ('/'.join(dirs[0:n]), i)
+    # if path != '':
+        # dirs = path.split('/')
+        # html_dir = '''
+        # <tr><td colspan=3>
+        # <ol class="breadcrumb">
+          # <li>
+            # <span class="filelist folder">
+              # <i class="glyphicon glyphicon-home" title="/"></i>
+            # </span>
+          # </li>
+          # '''
+        # for n, i in enumerate(dirs[:-1:], 1):
+            # print("/%s %s" % ('/'.join(dirs[0:n]), i))
             # html_dir += '''
-              # <li class="active">%s</li>
-            # </ol>
-            # </td></tr>''' % dirs[-1]
-            # path = '%s/' % path.strip('/')
-            # html_dir += '''
-            # <tr>
-              # <td><i class="glyphicon glyphicon-folder-close"></i></td>
-              # <td class="filelist folder" colspan=2 title="/%s..">..</td>
-            # </tr>''' % path
-        # for file in os.listdir('./static/mp4/%s' % path):
-            # if os.path.isdir('./static/mp4/%s%s' % (path, file)):
-                # html_dir += '''
-                # <tr>
-                  # <td><i class="glyphicon glyphicon-folder-close"></i></td>
-                  # <td class="filelist folder" title="/%s%s">%s</td>
-                  # <td class="move" title="%s%s">
-                    # <i class="glyphicon glyphicon-remove-circle" title="%s%s"></i>
-                  # </td>
-                # </tr>''' % (path, file, file, path, file, path, file)
-            # elif re.match('.*\.((?i)mp)4$', file):
-                # html_mp4 += '''
-                # <tr>
-                  # <td><i class="glyphicon glyphicon-film"></i></td>
-                  # <td class="filelist mp4" title="?src=%s%s">
-                    # %s<br><small>%s</small>
-                  # </td>
-                  # <td class="move" title="%s%s">
-                    # <i class="glyphicon glyphicon-remove-circle" title="%s%s"></i>
-                  # </td>
-                # </tr>
-                # ''' % (path, file, file, get_size(path + file), path, file, path, file)
-            # else:
-                # html_files += '''
-                # <tr>
-                  # <td><i class="glyphicon glyphicon-file"></i></td>
-                  # <td>
-                    # <span class="filelist other">%s</span>
-                    # <br><small>%s</small>
-                  # </td>
-                  # <td class="move" title="%s%s">
-                    # <i class="glyphicon glyphicon-remove-circle" title="%s%s"></i>
-                  # </td>
-                # </tr>''' % (file, get_size(path + file), path, file, path, file)
-        # return ''.join([html_dir, html_mp4, html_files])
-    # except Exception as e:
-        # abort(404, str(e))
+            # <li><span class="filelist folder" title="/%s">%s</span>
+            # </li>''' % ('/'.join(dirs[0:n]), i)
+        # html_dir += '''
+          # <li class="active">%s</li>
+        # </ol>
+        # </td></tr>''' % dirs[-1]
+        # path = '%s/' % path.strip('/')
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))  # set file path as current
-init_db()
+# Initialize DataBase
+run_sql('''create table if not exists history
+                (FILENAME text PRIMARY KEY not null,
+                TIME float not null,
+                DURATION float, LATEST_DATE datetime not null);''')
 
 if __name__ == '__main__':  # for debug
 
