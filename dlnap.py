ef2aca4a455caa47f35557382ef807bc22c37dfa
hwlu <haiwei.lu@99wuxian.com>
2017-06-26 17:30:51 +0800

    update dlnap

--- dlnap.py
+++ dlnap.py
@@ -34,6 +34,7 @@ import traceback
 import mimetypes
 from contextlib import contextmanager
 
+
 import os
 py3 = sys.version_info[0] == 3
 if py3:
@@ -51,7 +52,10 @@ import threading
 SSDP_GROUP = ("239.255.255.250", 1900)
 URN_AVTransport = "urn:schemas-upnp-org:service:AVTransport:1"
 URN_AVTransport_Fmt = "urn:schemas-upnp-org:service:AVTransport:{}"
+
+URN_RenderingControl = "urn:schemas-upnp-org:service:RenderingControl:1"
 URN_RenderingControl_Fmt = "urn:schemas-upnp-org:service:RenderingControl:{}"
+
 SSDP_ALL = "ssdp:all"
 
 # =================================================================================================
@@ -120,6 +124,7 @@ def _get_tag_value(x, i = 0):
    return (tag.strip(), value[:-1], x[i+1:])
 
 def _xml2dict(s, ignoreUntilXML = False):
+
    """ Convert xml to dictionary.
 
    <?xml version="1.0"?>
@@ -348,6 +353,7 @@ def _send_tcp(to, payload):
       if py3:
          data = data.decode('utf-8')
       data = _xml2dict(_unescape_xml(data), True)
+
       errorDescription = _xpath(data, 's:Envelope/s:Body/s:Fault/detail/UPnPError/errorDescription')
       if errorDescription is not None:
          logging.error(errorDescription)
@@ -356,6 +362,8 @@ def _send_tcp(to, payload):
    finally:
       sock.close()
    return data
+
+
 
 def _get_location_url(raw):
    """ Extract device description url from discovery response
@@ -391,6 +399,7 @@ class DlnapDevice:
       self.port = None
       self.name = 'Unknown'
       self.control_url = None
+      self.rendering_control_url = None
       self.has_av_transport = False
 
       try:
@@ -411,6 +420,9 @@ class DlnapDevice:
 
          self.control_url = _get_control_url(self.__desc_xml, URN_AVTransport)
          self.__logger.info('control_url: {}'.format(self.control_url))
+
+         self.rendering_control_url = _get_control_url(self.__desc_xml, URN_RenderingControl)
+         self.__logger.info('rendering_control_url: {}'.format(self.rendering_control_url))
 
          self.has_av_transport = self.control_url is not None
          self.__logger.info('=> Initialization completed'.format(ip))
@@ -446,11 +458,16 @@ class DlnapDevice:
       action -- control action
       data -- dictionary with XML fields value
       """
-
-      urn = URN_AVTransport_Fmt.format(self.ssdp_version)
+      if action in ["SetVolume", "SetMute", "GetVolume"]:
+          url = self.rendering_control_url
+          urn = URN_RenderingControl_Fmt.format(self.ssdp_version)
+      else:
+          url = self.control_url
+          urn = URN_AVTransport_Fmt.format(self.ssdp_version)
       payload = self._payload_from_template(action=action, data=data, urn=urn)
+
       packet = "\r\n".join([
-         'POST {} HTTP/1.1'.format(self.control_url),
+         'POST {} HTTP/1.1'.format(url),
          'User-Agent: {}/{}'.format(__file__, __version__),
          'Accept: */*',
          'Content-Type: text/xml; charset="utf-8"',
@@ -461,31 +478,7 @@ class DlnapDevice:
          '',
          payload,
          ])
-      self.__logger.debug(packet)
-      return packet
 
-   def _create_packet_rcs(self, action, data):
-      """ Create packet to send to device control url.
-
-      action -- control action
-      data -- dictionary with XML fields value
-      """
-
-      urn = URN_RenderingControl_Fmt.format(self.ssdp_version)
-      payload = self._payload_from_template(action=action, data=data, urn=urn)
-
-      packet = "\r\n".join([
-         'POST {} HTTP/1.1'.format('/upnp/service/RenderingControl/Control'),
-         'User-Agent: {}/{}'.format(__file__, __version__),
-         'Accept: */*',
-         'Content-Type: text/xml; charset="utf-8"',
-         'HOST: {}:{}'.format(self.ip, self.port),
-         'Content-Length: {}'.format(len(payload)),
-         'SOAPACTION: "{}#{}"'.format(urn, action),
-         'Connection: close',
-         '',
-         payload,
-         ])
       self.__logger.debug(packet)
       return packet
 
@@ -496,27 +489,6 @@ class DlnapDevice:
       instance_id -- device instance id
       """
       packet = self._create_packet('SetAVTransportURI', {'InstanceID':instance_id, 'CurrentURI':url, 'CurrentURIMetaData':'' })
-      _send_tcp((self.ip, self.port), packet)
-
-   def seek(self, progress, instance_id = 0):
-      """
-      xenos
-      """
-      packet = self._create_packet('Seek', {'InstanceID':instance_id, 'Unit':'REL_TIME', 'Target':progress })
-      _send_tcp((self.ip, self.port), packet)
-
-   def volume(self, vol, instance_id = 0):
-      """
-      xenos
-      """#SETMUTE
-      packet = self._create_packet_rcs('SetVolume', {'InstanceID':instance_id, 'DesiredVolume':vol, 'Channel':'Master'})
-      _send_tcp((self.ip, self.port), packet)
-
-   def get_volume(self, vol, instance_id = 0):
-      """
-      xenos
-      """#SETMUTE
-      packet = self._create_packet_rcs('GetVolume', {'InstanceID':instance_id, 'Channel':'Master'})
       _send_tcp((self.ip, self.port), packet)
 
    def play(self, instance_id = 0):
@@ -543,6 +515,49 @@ class DlnapDevice:
       packet = self._create_packet('Stop', {'InstanceID': instance_id, 'Speed': 1})
       _send_tcp((self.ip, self.port), packet)
 
+
+   def seek(self, position, instance_id = 0):
+      """
+      Seek position
+      """
+      packet = self._create_packet('Seek', {'InstanceID':instance_id, 'Unit':'REL_TIME', 'Target': position })
+      _send_tcp((self.ip, self.port), packet)
+
+
+   def volume(self, volume=10, instance_id = 0):
+      """ Stop media that is currently playing back.
+
+      instance_id -- device instance id
+      """
+      packet = self._create_packet('SetVolume', {'InstanceID': instance_id, 'DesiredVolume': volume, 'Channel': 'Master'})
+
+      _send_tcp((self.ip, self.port), packet)
+      
+      
+   def get_volume(self, vol, instance_id = 0):
+      """
+      get volume
+      """
+      packet = self._create_packet('GetVolume', {'InstanceID':instance_id, 'Channel': 'Master'})
+      _send_tcp((self.ip, self.port), packet)
+
+
+   def mute(self, instance_id = 0):
+      """ Stop media that is currently playing back.
+
+      instance_id -- device instance id
+      """
+      packet = self._create_packet('SetMute', {'InstanceID': instance_id, 'DesiredMute': '1', 'Channel': 'Master'})
+      _send_tcp((self.ip, self.port), packet)
+
+   def unmute(self, instance_id = 0):
+      """ Stop media that is currently playing back.
+
+      instance_id -- device instance id
+      """
+      packet = self._create_packet('SetMute', {'InstanceID': instance_id, 'DesiredMute': '0', 'Channel': 'Master'})
+      _send_tcp((self.ip, self.port), packet)
+
    def info(self, instance_id=0):
       """ Transport info.
 
@@ -559,13 +574,14 @@ class DlnapDevice:
       packet = self._create_packet('GetMediaInfo', {'InstanceID': instance_id})
       return _send_tcp((self.ip, self.port), packet)
 
-   def position_info(self, instance_id=0):
-      """ Media info.
 
+   def position_info(self, instance_id=0):
+      """ Position info.
       instance_id -- device instance id
       """
       packet = self._create_packet('GetPositionInfo', {'InstanceID': instance_id})
       return _send_tcp((self.ip, self.port), packet)
+
 
    def set_next(self, url):
       pass
@@ -636,6 +652,9 @@ if __name__ == '__main__':
       print(' --play <url> - set current url for play and start playback it. In case of url is empty - continue playing recent media.')
       print(' --pause - pause current playback')
       print(' --stop - stop current playback')
+      print(' --mute - mute playback')
+      print(' --unmute - unmute playback')
+      print(' --volume <vol> - set current volume for playback')
       print(' --timeout <seconds> - discover timeout')
       print(' --ssdp-version <version> - discover devices by protocol version, default 1')
       print(' --proxy - use local proxy on proxy port')
@@ -659,6 +678,10 @@ if __name__ == '__main__':
                                                                'play=',
                                                                'pause',
                                                                'stop',
+                                                               'volume=',
+                                                               'mute',
+                                                               'unmute',
+
 
                                                                # discover arguments
                                                                'list',
@@ -679,6 +702,7 @@ if __name__ == '__main__':
 
    device = ''
    url = ''
+   vol = 10
    timeout = 1
    action = ''
    logLevel = logging.WARN
@@ -722,6 +746,13 @@ if __name__ == '__main__':
          action = 'pause'
       elif opt in ('--stop'):
          action = 'stop'
+      elif opt in ('--volume'):
+         action = 'volume'
+         vol = arg
+      elif opt in ('--mute'):
+         action = 'mute'
+      elif opt in ('--unmute'):
+         action = 'unmute'
       elif opt in ('--info'):
          action = 'info'
       elif opt in ('--media-info'):
@@ -776,6 +807,12 @@ if __name__ == '__main__':
       d.pause()
    elif action == 'stop':
       d.stop()
+   elif action == 'volume':
+      d.volume(vol)
+   elif action == 'mute':
+      d.mute()
+   elif action == 'unmute':
+      d.unmute()
    elif action == 'info':
       print(d.info())
    elif action == 'media-info':
