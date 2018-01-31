72e2f99353ffa1aaa4d5ff9985cfad5e9af545e7
Xenocider <xenos.lu@gmail.com>
2018-01-31 20:32:24 +0800

    update

--- xlmp.py
+++ xlmp.py
@@ -284,7 +284,7 @@ def get_next_file(src):
 @check_dmr_exist
 def dlna_load(src):
     """request for load Video through DLNA"""
-    if not os.path.exists(os.path.join(VIDEO_PATH, src)):
+    if not os.path.exists('%s/%s' % (VIDEO_PATH, src)):
         logging.warning('File not found: %s' % src)
         return 'Error: File not found.'
     logging.info('start loading... tracker state:%s' % tracker.state)
