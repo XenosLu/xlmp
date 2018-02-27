dd8398e89227c6c631d54fd0aec2f732caf7f925
hwlu <haiwei.lu@99wuxian.com>
2018-02-27 13:20:44 +0800

    update

--- xlmp.py
+++ xlmp.py
@@ -532,7 +532,8 @@ Handlers=[
     (r'/save/(?P<src>.*)', SaveHandler),
     (r'/play/(?P<src>.*)', WebPlayerHandler),
     (r'/video/(.*)', tornado.web.StaticFileHandler, {'path': VIDEO_PATH}),
-    (r'/(apple-touch-icon\.png)', tornado.web.StaticFileHandler, {'path': 'static'})
+    (r'/(apple-touch-icon\.png)', tornado.web.StaticFileHandler, {'path': 'static'}),
+    (r'/(favicon\.ico)', tornado.web.StaticFileHandler, {'path': 'static'})
 ]
 
 settings = {
