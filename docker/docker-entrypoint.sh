159f93b4f25490868c644852843a468b76a9b05d
hwlu <haiwei.lu@99wuxian.com>
2018-03-07 09:38:25 +0800

    test

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,8 +3,7 @@
 set -e
 
 # exec something
-
-exec /usr/bin/python3 /xlmp/xlmp.py &
-exec /usr/sbin/nginx -g 'daemon off;'
+exec /usr/sbin/nginx -g 'daemon off;' &
+exec /usr/bin/python3 /xlmp/xlmp.py
 
 exec "$@"
