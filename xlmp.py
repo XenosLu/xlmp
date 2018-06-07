ff4b3e2037e84d832b05ac2930a217977e86dbc4
hwlu <haiwei.lu@99wuxian.com>
2018-06-07 09:07:35 +0800

    update

--- xlmp.py
+++ xlmp.py
@@ -611,7 +611,7 @@ class TestHandler(tornado.web.RequestHandler):
 
 class DlnaWebSocketHandler(tornado.websocket.WebSocketHandler):
     """DLNA info retriever use web socket"""
-    executor = ThreadPoolExecutor(9)
+    executor = ThreadPoolExecutor(20)
     _running = True
 
     # def data_received(self, chunk):
