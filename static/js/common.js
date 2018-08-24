b1d79051bc492730beb26f7241954e7508941edb
hwlu <haiwei.lu@99wuxian.com>
2018-08-24 13:43:44 +0800

    updated

--- static/js/common.js
+++ static/js/common.js
@@ -91,9 +91,9 @@ hammertimeModal.on("swiperight", function (ev) {
     if (window.commonView.swipeState > 1)
         window.commonView.swipeState = 1;
 });
-hammertimeModal.add( new Hammer.Press({time: 1500 }) )
-hammertimeModal.on("press", function (ev) {
-    out("press");
+hammertimeModal.add( new Hammer.Press({time: 1000 }) )
+hammertimeModal.on("pressup", function (ev) {
+    out("pressup");
     var target;
     // if(ev.target.tagName=='TD')
         // target = ev.target;
