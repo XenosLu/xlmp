eab174b990b3fe583d7348ad04e573ab6b388353
hwlu <haiwei.lu@99wuxian.com>
2018-09-05 09:27:31 +0800

    updated

--- static/js/common.js
+++ static/js/common.js
@@ -1,1 +1,24 @@
 "use strict";
+/**
+ * Convert Time format to seconds
+ *
+ * @method timeToSecond
+ * @param {String} time
+ * @return {Integer}
+ */
+function timeToSecond(time) {
+    var t = String(time).split(":");
+    return (parseInt(t[0]) * 3600 + parseInt(t[1]) * 60 + parseInt(t[2]));
+}
+
+/**
+ * Convert Second to Time Format
+ *
+ * @method secondToTime
+ * @param {Integer} time
+ * @return {String}
+ */
+function secondToTime(time) {
+    return ("0" + Math.floor(time / 3600)).slice(-2) + ":" +
+    ("0" + Math.floor(time % 3600 / 60)).slice(-2) + ":" + (time % 60 / 100).toFixed(2).slice(-2);
+}
--- static/js/main.js
+++ static/js/main.js
@@ -26,7 +26,6 @@ window.commonView = new Vue({
             wp_src: '',
             history: [],
             filelist: [],
-            mode: 'WebPlayer',
             positionBar: {
                 min: 0,
                 max: 0,
@@ -320,29 +319,8 @@ function out(text) {
     };
 }
 
-/**
- * Convert Second to Time Format
- *
- * @method secondToTime
- * @param {Integer} time
- * @return {String}
- */
-function secondToTime(time) {
-    return ("0" + Math.floor(time / 3600)).slice(-2) + ":" +
-    ("0" + Math.floor(time % 3600 / 60)).slice(-2) + ":" + (time % 60 / 100).toFixed(2).slice(-2);
-}
 
-/**
- * Convert Time format to seconds
- *
- * @method timeToSecond
- * @param {String} time
- * @return {Integer}
- */
-function timeToSecond(time) {
-    var t = String(time).split(":");
-    return (parseInt(t[0]) * 3600 + parseInt(t[1]) * 60 + parseInt(t[2]));
-}
+
 
 function dlna_add_touch() {
     var hammertimeDlna = new Hammer(document.getElementById("DlnaTouch"));
