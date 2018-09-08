434e4f1b3158c7c62703b2bd0a9bd219adcb957f
Xenocider <xenos.lu@gmail.com>
2018-09-08 16:03:53 +0800

    updated

--- xlmp.py
+++ xlmp.py
@@ -304,6 +304,7 @@ class DMRTracker_coroutine(Thread):
             self.dmr.stop()
             logging.info('Waiting for DMR stopped...')
             sleep(0.75)
+            sleep(0.73)
         # try:
         if self.dmr.set_current_media(url):
             logging.info('Loaded %s', unquote(url))
