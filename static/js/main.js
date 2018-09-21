b1af8d4af0949bc8c9e2df82572e69f9dba4d32b
hwlu <haiwei.lu@99wuxian.com>
2018-09-21 18:14:38 +0800

    updated

--- static/js/main.js
+++ static/js/main.js
@@ -406,18 +406,18 @@ var ws_link = dlnalink();
 setInterval("ws_link.check()", 1200);
 // modalTouch();
 
-// window.onload=function () {
-    // document.addEventListener('touchstart',function (event) {
-        // if(event.touches.length>1){
-            // event.preventDefault();
-        // }
-    // })
-    // var lastTouchEnd=0;
-    // document.addEventListener('touchend',function (event) {
-        // var now=(new Date()).getTime();
-        // if(now-lastTouchEnd<=300){
-            // event.preventDefault();
-        // }
-        // lastTouchEnd=now;
-    // },false)
-// }
+window.onload=function () {
+    document.addEventListener('touchstart',function (event) {
+        if(event.touches.length>1){
+            event.preventDefault();
+        }
+    })
+    var lastTouchEnd=0;
+    document.addEventListener('touchend',function (event) {
+        var now=(new Date()).getTime();
+        if(now-lastTouchEnd<=300){
+            event.preventDefault();
+        }
+        lastTouchEnd=now;
+    },false)
+}
