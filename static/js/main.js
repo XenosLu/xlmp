c6d8268b2b9e7061f197f5a81784f15ee3bd6811
hwlu <haiwei.lu@99wuxian.com>
2018-09-21 13:29:17 +0800

    updated

--- static/js/main.js
+++ static/js/main.js
@@ -6,22 +6,22 @@ var icon = {
     "other": "oi-file"
 };
 
-function vueTouch(el,type,binding){
+function vueTouch(el, type, binding) {
     this.el = el;
     this.type = type;
     this.binding = binding;
     var hammertime = new Hammer(this.el);
-    hammertime.on(this.type,this.binding.value);
+    hammertime.on(this.type, this.binding.value);
 };
-Vue.directive("tap",{
-    bind:function(el,binding){
-        new vueTouch(el,"tap",binding);
+Vue.directive("tap", {
+    bind: function (el, binding) {
+        new vueTouch(el, "tap", binding);
     }
 });
 
-Vue.directive("doubletap",{
-    bind:function(el,binding){
-        new vueTouch(el,"doubletap",binding);
+Vue.directive("press", {
+    bind: function (el, binding) {
+        new vueTouch(el, "press", binding);
     }
 });
 
