fbfaa664f959db51c084deff54bc3c4bc0c2bfd2
Xenocider <xenos.lu@gmail.com>
2018-09-21 20:30:56 +0800

    updated

--- static/js/main.js
+++ static/js/main.js
@@ -97,6 +97,7 @@ window.appView = new Vue({
         delimiters: ['${', '}'],
         el: '#v-main',
         data: {
+            devMode: true, // develop mode
             editMode: false,
             video: {
                 lastplaytime: 0,
@@ -386,24 +387,24 @@ window.appView = new Vue({
                 document.onmousemove = this.showFixBar;
             }
             axios.defaults.timeout = 1999;
+            // prevent double click for IOS
+            document.addEventListener('touchstart', function (event) {
+                if (event.touches.length > 1) {
+                    event.preventDefault();
+                }
+            })
+            var lastTouchEnd = 0;
+            document.addEventListener('touchend', function (event) {
+                var now = (new Date()).getTime();
+                if (now - lastTouchEnd <= 300) {
+                    event.preventDefault();
+                }
+                lastTouchEnd = now;
+            }, false)
         },
     });
 
 var ws_link = dlnalink();
 setInterval("ws_link.check()", 1200);
 
-window.onload=function () {
-    document.addEventListener('touchstart',function (event) {
-        if(event.touches.length>1){
-            event.preventDefault();
-        }
-    })
-    var lastTouchEnd=0;
-    document.addEventListener('touchend',function (event) {
-        var now=(new Date()).getTime();
-        if(now-lastTouchEnd<=300){
-            event.preventDefault();
-        }
-        lastTouchEnd=now;
-    },false)
-}
+
