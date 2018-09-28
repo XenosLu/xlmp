fa0c0330c5eb38f02f4653166d7abadd0c9edd12
Xenocider <xenos.lu@gmail.com>
2018-09-28 21:10:49 +0800

    removed reduntent tests

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@
 import unittest
 import json
 from tornado.testing import AsyncHTTPTestCase
-from xlmp import APP, TRACKER, LOADER
+from xlmp import APP, TRACKER
 
 class TestMain(AsyncHTTPTestCase):
     """test class"""
@@ -15,59 +15,12 @@ class TestMain(AsyncHTTPTestCase):
         response = self.fetch('/')
         self.assertEqual(response.code, 200)
 
-    def test_main_dlna(self):
-        response = self.fetch('/dlna')
-        self.assertEqual(response.code, 200)
-
-    def test_fs_ls(self):
-        response = self.fetch('/fs/ls/')
-        self.assertEqual(response.code, 200)
-        self.assertEqual(type(json.loads(response.body.decode())), dict)
-
-    def test_fs_move(self):
-        response = self.fetch('/fs/move/test')
-        self.assertEqual(response.code, 404)
-        # self.assertEqual(response.body, b'Hello, world')
-
-    def test_hist(self):
-        response = self.fetch('/hist/ls')
-        self.assertEqual(response.code, 200)
-        self.assertEqual(type(json.loads(response.body.decode())), dict)
-        response = self.fetch('/hist/test')
-        self.assertEqual(response.code, 404)
-
-        response = self.fetch('/sys/backup')
-        self.assertEqual(response.code, 200)
-        response = self.fetch('/hist/clear')
-        self.assertEqual(response.code, 200)
-        response = self.fetch('/sys/restore')
-        self.assertEqual(response.code, 200)
-        # response = self.fetch('/hist/rm')
-
-    def test_sys(self):
-        response = self.fetch('/sys/test')
-        self.assertEqual(response.code, 403)
+    def test_api(self):
+        response = self.fetch('/api')
+        self.assertEqual(response.code, 405)
+        print(response.code)
         print(response.body)
-        print('*'*80)
-        # response = self.fetch('/hist/clear')
-        # response = self.fetch('/hist/rm')
 
-
-
-
-    # (r'/sys/(?P<opt>\w*)', SystemCommandHandler),
-
-    # (r'/dlna/link', DlnaWebSocketHandler),
-    # (r'/dlna/info', DlnaInfoHandler),
-    # (r'/dlna/setdmr/(?P<dmr>.*)', SetDmrHandler),
-    # (r'/dlna/searchdmr', SearchDmrHandler),
-    # (r'/dlna/vol/(?P<opt>\w*)', DlnaVolumeControlHandler),
-    # (r'/dlna/next', DlnaNextHandler),
-    # (r'/dlna/load/(?P<src>.*)', DlnaLoadHandler),
-    # (r'/dlna/(?P<opt>\w*)/?(?P<progress>.*)', DlnaHandler),
-
-    # (r'/wp/save/(?P<src>.*)', SaveHandler),
-    # (r'/wp/play/(?P<src>.*)', WebPlayerHandler),
 
 if __name__ == '__main__':
     unittest.main()
