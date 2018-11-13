c02e302d04d01574ac87228ec333121f2ebf897f
hwlu <haiwei.lu@99wuxian.com>
2018-11-13 12:08:51 +0800

    updated

---
+++ static/js/demo.js
@@ -0,0 +1,82 @@
+window.appView.history = [{
+        'filename': 'test.mp4',
+        'fullpath': 'test.mp4',
+        'position': 436.958665,
+        'duration': 983.365079,
+        'latest_date': '2018-10-15 14:03:30',
+        'path': '',
+        'exist': true
+    }, {
+        'filename': '卢克凯奇.Marvels.Luke.Cage.S02E12.中英字幕.WEBrip.720P-人人影视.mp4',
+        'fullpath': 'luke.cage.s02/卢克凯奇.Marvels.Luke.Cage.S02E12.中英字幕.WEBrip.720P-人人影视.mp4',
+        'position': 1402.0,
+        'duration': 3801.0,
+        'latest_date': '2018-10-14 12:51:22',
+        'path': 'luke.cage.s02',
+        'exist': false
+    }, {
+        'filename': '卢克凯奇.Marvels.Luke.Cage.S02E10.中英字幕.WEBrip.720P-人人影视.mp4',
+        'fullpath': 'luke.cage.s02/卢克凯奇.Marvels.Luke.Cage.S02E10.中英字幕.WEBrip.720P-人人影视.mp4',
+        'position': 3497.0,
+        'duration': 3615.0,
+        'latest_date': '2018-10-14 10:27:07',
+        'path': 'luke.cage.s02',
+        'exist': false
+    }, {
+        'filename': '氪星.Krypton.S01E05.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'fullpath': '氪星.Krypton.S01.中英字幕.HDTVrip.720P/氪星.Krypton.S01E05.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'position': 1042.0,
+        'duration': 2555.0,
+        'latest_date': '2018-08-23 20:53:57',
+        'path': '氪星.Krypton.S01.中英字幕.HDTVrip.720P',
+        'exist': false
+    }, {
+        'filename': '超女.Supergirl.S03E06.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'fullpath': 'oct/超女.Supergirl.S03E06.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'position': 101.0,
+        'duration': 2572.0,
+        'latest_date': '2017-12-22 20:46:46',
+        'path': 'oct',
+        'exist': false
+    }, {
+        'filename': '天赋异禀.The.Gifted.S01E10.中英字幕.HDTVrip.720p-人人影视.mp4',
+        'fullpath': 'oct/天赋异禀.The.Gifted.S01E10.中英字幕.HDTVrip.720p-人人影视.mp4',
+        'position': 1068.0,
+        'duration': 2645.0,
+        'latest_date': '2017-12-21 21:45:58',
+        'path': 'oct',
+        'exist': false
+    }, {
+        'filename': '闪电侠.The.Flash.S04E03.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'fullpath': 'oct/闪电侠.The.Flash.S04E03.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'position': 13.0,
+        'duration': 2598.0,
+        'latest_date': '2017-12-02 16:17:27',
+        'path': 'oct',
+        'exist': false
+    }, {
+        'filename': '魔法师.The.Magicians.US.S02E03.720p.HDTV.x264.双语字幕精校版-深影字幕组.mp4',
+        'fullpath': '魔法师.The.Magicians.US.S02E0/魔法师.The.Magicians.US.S02E03.720p.HDTV.x264.双语字幕精校版-深影字幕组.mp4',
+        'position': 17.0,
+        'duration': 2437.0,
+        'latest_date': '2017-11-29 14:23:42',
+        'path': '魔法师.The.Magicians.US.S02E0',
+        'exist': false
+    }, {
+        'filename': '明日传奇.DCs.Legends.of.Tomorrow.S03E02.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'fullpath': 'oct/明日传奇.DCs.Legends.of.Tomorrow.S03E02.中英字幕.HDTVrip.720P-人人影视.mp4',
+        'position': 2598.289528,
+        'duration': 2598.677333,
+        'latest_date': '2017-11-26 09:11:44',
+        'path': 'oct',
+        'exist': false
+    }, {
+        'filename': '绿箭.Arrow.S06E03中英字幕.HDTVrip.720P-人人影视.mp4',
+        'fullpath': 'oct/绿箭.Arrow.S06E03中英字幕.HDTVrip.720P-人人影视.mp4',
+        'position': 2568.551935,
+        'duration': 2569.567,
+        'latest_date': '2017-11-22 11:58:30',
+        'path': 'oct',
+        'exist': false
+    }
+];
